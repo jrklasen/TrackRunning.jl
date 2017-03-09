63a44ab0b39fdba9581094cd75d00a0676741468
Jonas Klasen <jrklasen@gmx.de>
2017-03-09 15:26:09 +0100

    Map plotting finction

---
+++ src/plot.jl
@@ -0,0 +1,13 @@
+"""
+    `mapshow(run)` Plotting of map for a given data set.
+"""
+function mapshow(run::Run, minzoom::Integer = 0, maxzoom::Integer = 21,
+    mapservice::AbstractString="google", maptype::AbstractString="roadmap")
+    mapcoor = mapprojection(run)
+    mapfile = downloadmap(mapcoor)
+    runmap = permuteddimsview(rawview(channelview(load(mapfile))), (2, 3, 1)) / 256
+    xylim = [mapcoor.lon_left, mapcoor.lon_right, mapcoor.lat_bottom, mapcoor.lat_top]
+    xticks([])
+    yticks([])
+    imshow(runmap, origin="upper", extent=xylim, aspect=1.5)
+end
