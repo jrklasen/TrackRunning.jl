ed5f7517c7a49fd3afc82d989f98e669f9d9d244
Jonas Klasen <jrklasen@gmx.de>
2017-03-16 10:48:35 +0100

    add a internal constructor for type

--- src/run.jl
+++ src/run.jl
@@ -1,8 +1,30 @@
 immutable Run
     date::Vector{DateTime}
-    lap::Vector{Integer}
     lon::Vector{AbstractFloat}
     lat::Vector{AbstractFloat}
     ele::Vector{AbstractFloat}
+    lap::Vector{Integer}
     hr::Vector{Integer}
+    function Run(date, lon, lat,
+        ele=Vector{AbstractFloat}[], lap=Vector{Integer}[], hr=Vector{Integer}[])
+        n = length(lon)
+        if length(lat) != n
+            error("'lon' and 'lat' differ in length")
+        end
+        if length(date) != n
+            error("'data' has incorrect length")
+        end
+        if length(lap) == 0
+            lap = one(n)
+        elseif length(lap) != n
+            error("'lap' has incorrect length")
+        end
+        if  length(ele) != n & length(ele) != 0
+            error("'ele' has incorrect length")
+        end
+        if  length(hr) != n & length(hr) != 0
+            error("'hr' has incorrect length")
+        end
+        new(date, lon, lat, ele, lap, hr)
+    end
 end
