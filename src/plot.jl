8fc7723c98f2fce1b60f6c83c544678a8da0f8d9
Jonas Klasen <jrklasen@gmx.de>
2017-06-25 18:04:11 +0200

    new plotrun function

--- src/plot.jl
+++ src/plot.jl
@@ -11,3 +11,12 @@ function showmap(run::Run, minzoom::Integer = 0, maxzoom::Integer = 21,
     yticks([])
     imshow(runmap, origin="upper", extent=xylim, aspect=1.5)
 end
+
+"""
+    `plotrun(run::Run)` plot running data.
+"""
+function plotrun(run::Run)
+    kmh = speed_kmh(run)
+    kmh = kmh / maximum(kmh)
+    scatter(run.lon[2:end], run.lat[2:end], c = kmh, cmap = ColorMap(:coolwarm), s = 1)
+end
